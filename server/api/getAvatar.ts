8ebb6b41618af9caebeb27753ae47ff755c6e1ed
XiYang6666 <1782356858@qq.com>
2024-05-12 16:49:01 +0800

    :sparkles: improve getAvatar

--- server/api/getAvatar.ts
+++ server/api/getAvatar.ts
@@ -2,20 +2,6 @@ import { getGravatarUrl } from "../getGravatarUrl";
 
 const config = useRuntimeConfig();
 
-let avatarCache: {
-    time: number;
-    value: Blob;
-};
-
 export default defineEventHandler(async (event) => {
-    if (avatarCache && avatarCache.time + config.avatarCacheTime >= Date.now()) {
-        return avatarCache.value;
-    }
-    const avatarUrl = getGravatarUrl(config.ownerEmail);
-    const imageBlob: Blob = await $fetch(avatarUrl);
-    avatarCache = {
-        time: Date.now(),
-        value: imageBlob,
-    };
-    return imageBlob;
+    return sendRedirect(event, getGravatarUrl(config.ownerEmail))
 });
