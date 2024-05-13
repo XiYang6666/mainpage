68a3994473640d0dc87b2c55b2b21f68ed33bc75
XiYang6666 <1782356858@qq.com>
2024-05-13 10:46:51 +0800

    :sparkles: fix die code

--- server/api/getAvatar.ts
+++ server/api/getAvatar.ts
@@ -3,5 +3,5 @@ import { getGravatarUrl } from "../getGravatarUrl";
 const config = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
-    return sendRedirect(event, getGravatarUrl(config.ownerEmail))
+    return sendRedirect(event, getGravatarUrl(config.ownerEmail).toString());
 });
--- server/getGravatarUrl.ts
+++ server/getGravatarUrl.ts
@@ -5,5 +5,5 @@ export function getGravatarUrl(email: string, size: number = 256) {
 
     const trimmedEmail = email.trim().toLowerCase();
     const hash = createHash("sha256").update(trimmedEmail).digest("hex");
-    return `${runtimeConfig.gravatarUrl}/avatar/${hash}?s=${size}&d=identicon`;
+    return new URL(`/avatar/${hash}?s=${size}&d=identicon}`, runtimeConfig.gravatarUrl);
 }
